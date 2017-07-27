3bbac875c1c11fee51b953d450902fde72e29c3c
Kito Cheng <kito@andestech.com>
2017-07-27 16:44:22 +0800

    Add RISC-V port for newlib
      Contributor list:
        - Andrew Waterman  <andrew@sifive.com>
        - Palmer Dabbelt  <palmer@dabbelt.com>
        - Kito Cheng  <kito.cheng@gmail.com>
        - Scott Beamer  <sbeamer@eecs.berkeley.edu>

---
+++ newlib/libc/machine/riscv/ieeefp.c
@@ -0,0 +1,99 @@
+#include <ieeefp.h>
+
+#ifdef __riscv_flen
+static void
+fssr(unsigned value)
+{
+  asm volatile ("fssr %0" :: "r"(value));
+}
+
+static unsigned
+frsr()
+{
+  unsigned value;
+  asm volatile ("frsr %0" : "=r" (value));
+  return value;
+}
+
+static fp_rnd
+frm_fp_rnd (unsigned frm)
+{
+  switch (frm)
+    {
+    case 0: return FP_RN;
+    case 1: return FP_RZ;
+    case 2: return FP_RM;
+    case 3: return FP_RP;
+    /* 4 ~ 7 is invalid value, so just retun FP_RP.  */
+    default:return FP_RP;
+    }
+}
+
+#endif
+
+fp_except
+fpgetmask(void)
+{
+  return 0;
+}
+
+fp_rnd
+fpgetround(void)
+{
+#ifdef __riscv_flen
+  unsigned rm = (frsr () >> 5) & 0x7;
+  return frm_fp_rnd (rm);
+#else
+  return FP_RZ;
+#endif
+}
+
+fp_except
+fpgetsticky(void)
+{
+#ifdef __riscv_flen
+  return frsr () & 0x1f;
+#else
+  return 0;
+#endif
+}
+
+fp_except
+fpsetmask(fp_except mask)
+{
+  return -1;
+}
+
+fp_rnd
+fpsetround(fp_rnd rnd_dir)
+{
+#ifdef __riscv_flen
+  unsigned fsr = frsr ();
+  unsigned rm = (fsr >> 5) & 0x7;
+  unsigned new_rm;
+  switch (rnd_dir)
+    {
+    case FP_RN: new_rm = 0;
+    case FP_RZ: new_rm = 1;
+    case FP_RM: new_rm = 2;
+    case FP_RP: new_rm = 3;
+    default:    return -1;
+    }
+  fssr (new_rm << 5 | fsr & 0x1f);
+  return frm_fp_rnd (rm);
+#else
+  return -1;
+#endif
+}
+
+fp_except
+fpsetsticky(fp_except sticky)
+{
+#ifdef __riscv_flen
+  unsigned fsr = frsr ();
+  fssr (sticky & 0x1f | fsr & ~0x1f);
+  return fsr & 0x1f;
+#else
+  return -1;
+#endif
+}
