b1461f746032ab59ec955ee32f57b9d683f8e84f
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-03-04 02:14:40 -0800

    Added more lib routines and fixed some bugs

--- libgloss/riscv/sys_fstat.c
+++ libgloss/riscv/sys_fstat.c
@@ -1,15 +1,21 @@
-#include <machine/syscall.h>
-#include "kernel_stat.h"
-#include "internal_syscall.h"
-
-/* Status of an open file. The sys/stat.h header file required is
-   distributed in the include subdirectory for this C library.  */
+#include <sys/stat.h>
+#include <bsg_newlib_fs.h>
+#include <bsg_newlib_fdtable.h>
 
 int
 _fstat(int file, struct stat *st)
 {
-  struct kernel_stat kst;
-  int rv = syscall_errno (SYS_fstat, file, &kst, 0, 0, 0, 0);
-  _conv_stat (st, &kst);
-  return rv;
+  lfs_file_t *fptr;
+  struct lfs_info finfo;
+
+  if(bsg_newlib_check_fd(file) < 0)
+    return -1;
+  else
+    fptr = bsg_newlib_get_file(file);
+
+  st->st_mode = S_IFREG;
+  st->st_size = (off_t) fptr->size;
+  st->st_blksize = (blksize_t) bsg_newlib_fs_cfg.block_size;
+  st->st_blocks  = (blkcnt_t) bsg_newlib_fs_cfg.block_count;
+  return 0;
 }
