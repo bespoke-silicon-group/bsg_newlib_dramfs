aa68ad5bfa4cacaa9672dcde2cfed7bcb297cf7b
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-03-25 06:01:08 -0700

    Added bsg newlib init routine

--- libgloss/riscv/bsg_newlib_fs/bsg_newlib_fs.h
+++ libgloss/riscv/bsg_newlib_fs/bsg_newlib_fs.h
@@ -3,14 +3,6 @@
 
 #include "lfs.h"
 
-// Toplevel filesystem struct
-// File system must be mounted to this struct by crt before calling main
-extern lfs_t bsg_newlib_fs;
-
-// File system configuration structure
-// Defined by the library based on above macros
-extern struct lfs_config bsg_newlib_fs_cfg;
-
 // These macros define cache sizes for file system operations.
 // Can be tweaked to improve file system performace
 #ifndef BSG_NEWLIB_FS_READ_SIZE
@@ -28,7 +20,18 @@ extern struct lfs_config bsg_newlib_fs_cfg;
 #define BSG_NEWLIB_FS_BLOCK_SIZE 128
 #endif
 #ifndef BSG_NEWLIB_FS_BLOCK_COUNT
-#define BSG_NEWLIB_FS_BLOCK_COUNT 16
+#define BSG_NEWLIB_FS_BLOCK_COUNT 64
 #endif
+
+// Toplevel filesystem struct
+// File system must be mounted to this struct by crt before calling main
+extern lfs_t bsg_newlib_fs;
+
+// File system configuration structure
+// Defined by the library based on above macros
+extern struct lfs_config bsg_newlib_fs_cfg;
+
+// File system init routine
+int bsg_newlib_fs_init(void);
 
 #endif // BSG_NEWLIB_FS_H
