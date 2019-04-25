efbbc90018c22cc7481007dded0a754001383a0e
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-04-24 20:34:00 -0700

    Added init routines required by newlib

---
+++ libgloss/riscv/bsg_newlib_init.c
@@ -0,0 +1,15 @@
+#include "bsg_newlib_init.h"
+#include "bsg_newlib_fs.h"
+
+int bsg_newlib_init(void) {
+  bsg_set_tile_x_y();
+
+  // Only bottom left tile handles the filesystem
+  if((__bsg_x == 0) && (__bsg_y == __bsg_tiles_Y-1)) {
+    if(bsg_newlib_fs_init() < 0) {
+      return -1;
+    }
+  }
+
+  return 0;
+}
