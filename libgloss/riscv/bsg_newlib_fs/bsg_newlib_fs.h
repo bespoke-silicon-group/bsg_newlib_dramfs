f1dbc6a075cc688ad06e08a56db56c3d19afde49
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-04-29 11:20:33 -0700

    Externed FS config so that block size and count are configurable

--- libgloss/riscv/bsg_newlib_fs/bsg_newlib_fs.h
+++ libgloss/riscv/bsg_newlib_fs/bsg_newlib_fs.h
@@ -3,32 +3,10 @@
 
 #include "lfs.h"
 
-// These macros define cache sizes for file system operations.
-// Can be tweaked to improve file system performace
-#ifndef BSG_NEWLIB_FS_READ_SIZE
-#define BSG_NEWLIB_FS_READ_SIZE 32
-#endif
-#ifndef BSG_NEWLIB_FS_PROG_SIZE
-#define BSG_NEWLIB_FS_PROG_SIZE 32
-#endif
-#ifndef BSG_NEWLIB_FS_LOOKAHEAD
-#define BSG_NEWLIB_FS_LOOKAHEAD 32
-#endif
-
-// File system size parameters
-#ifndef BSG_NEWLIB_FS_BLOCK_SIZE
-#define BSG_NEWLIB_FS_BLOCK_SIZE 128
-#endif
-#ifndef BSG_NEWLIB_FS_BLOCK_COUNT
-#define BSG_NEWLIB_FS_BLOCK_COUNT 64
-#endif
-
 // Toplevel filesystem struct
-// File system must be mounted to this struct by crt before calling main
-extern lfs_t bsg_newlib_fs;
+lfs_t bsg_newlib_fs;
 
 // File system configuration structure
-// Defined by the library based on above macros
 extern struct lfs_config bsg_newlib_fs_cfg;
 
 // File system init routine
