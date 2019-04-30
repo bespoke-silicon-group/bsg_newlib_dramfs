c98af4e24c1c9943b65d34da41051da4ac5daa57
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-04-30 14:34:56 -0700

    Open stdio files during newlib init

--- libgloss/riscv/bsg_newlib_init.c
+++ libgloss/riscv/bsg_newlib_init.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "bsg_newlib_init.h"
 #include "bsg_newlib_fs.h"
 
@@ -7,6 +8,15 @@ int bsg_newlib_init(void) {
   // Only bottom left tile handles the filesystem
   if((__bsg_x == 0) && (__bsg_y == __bsg_tiles_Y-1)) {
     if(bsg_newlib_fs_init() < 0) {
+      return -1;
+    }
+
+    // STDIO Files
+    stdin = fopen("stdin", "r");
+    stdout = fopen("stdout", "w");
+    stderr = fopen("stderr", "w");
+    
+    if(stdin == NULL || stdout == NULL || stderr == NULL) {
       return -1;
     }
   }
