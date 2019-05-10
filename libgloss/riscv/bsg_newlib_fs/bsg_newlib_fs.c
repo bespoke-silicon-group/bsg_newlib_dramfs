61081ff254b15fb3c6cb7916d0bb51e5a04b0d3a
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-05-09 17:28:56 -0700

    Externed init and exit routines

--- libgloss/riscv/bsg_newlib_fs/bsg_newlib_fs.c
+++ libgloss/riscv/bsg_newlib_fs/bsg_newlib_fs.c
@@ -1,3 +1,5 @@
+#include <unistd.h>
+#include <fcntl.h>
 #include "lfs.h"
 #include "lfs_bd.h"
 #include "bsg_newlib_fs.h"
@@ -19,6 +21,19 @@ int bsg_newlib_fs_init() {
 	if(lfs_mount(&bsg_newlib_fs, &bsg_newlib_fs_cfg) < 0) {
 		return -1;
 	}
+
+  // Open stdio files
+  if(open("stdin", O_RDONLY) != 0) { 
+    return -1;
+  }
+
+  if(open("stdout", O_WRONLY) != 1) {
+    return -1;
+  }
+
+  if(open("stderr", O_WRONLY) != 2) {
+    return -1;
+  }
 
 	return 0;
 }
--- libgloss/riscv/sys_exit.c
+++ libgloss/riscv/sys_exit.c
@@ -1,23 +1,9 @@
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdnoreturn.h>
 #include "bsg_newlib_fs.h"
-#include "../../../../../bsg_manycore_lib/bsg_manycore.h"
 
 /* Exit a program without cleaning up files.  */
 void
-_exit(int exit_status)
-{
-  //bsg_printf("Exiting\n");
-
-  close(0);
-  close(1);
-  close(2);
-
-  if(exit_status == EXIT_SUCCESS) {
-    bsg_finish();
-  } else {
-    bsg_fail();
-  }
-
-  bsg_wait_while(1);
+_exit(int exit_status) {
+  bsg_newlib_exit(exit_status);
+  while(1);
 }
