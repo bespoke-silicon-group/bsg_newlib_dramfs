2218580718e271df0f4a4ef41c903bf8a3ad8ad5
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-05-09 17:28:56 -0700

    Externed init and exit routines

--- libgloss/riscv/bsg_newlib_fs/bsg_newlib_fs.h
+++ libgloss/riscv/bsg_newlib_fs/bsg_newlib_fs.h
@@ -9,14 +9,8 @@ lfs_t bsg_newlib_fs;
 // File system configuration structure
 extern struct lfs_config bsg_newlib_fs_cfg;
 
-// File system init routine
-int bsg_newlib_fs_init(void);
-
-extern int __bsg_x;
-extern int __bsg_y;
-extern int __bsg_tiles_Y;
-
-#define bsg_x __bsg_x
-#define bsg_y __bsg_y
+// File system init & exit routines
+extern int bsg_newlib_fs_init(void);
+extern void bsg_newlib_exit(int);
 
 #endif // BSG_NEWLIB_FS_H
