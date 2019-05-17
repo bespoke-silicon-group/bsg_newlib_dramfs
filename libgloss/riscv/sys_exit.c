d2d89cf7ec7b584b3a879633e98046d5378937f3
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-05-16 17:46:53 -0700

    Close stdio files during exit

--- libgloss/riscv/sys_exit.c
+++ libgloss/riscv/sys_exit.c
@@ -1,9 +1,14 @@
-#include <stdnoreturn.h>
+#include <unistd.h>
 #include "bsg_newlib_fs.h"
 
 /* Exit a program without cleaning up files.  */
 void
 _exit(int exit_status) {
+  // close stdio
+  close(0);
+  close(1);
+  close(2);
+
   bsg_newlib_exit(exit_status);
   while(1);
 }
