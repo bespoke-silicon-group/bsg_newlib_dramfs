c5966b2dc85a7d882c3b45743a5d2cb4c84abb60
Dan Petrisko <petrisko@cs.washington.edu>
2021-11-06 23:43:17 -0700

    Adding dramfs_getchar

--- libgloss/dramfs/sys_read.c
+++ libgloss/dramfs/sys_read.c
@@ -10,6 +10,25 @@ ssize_t _read(int fd, void *ptr, size_t len)
     return -1;
   }
 
+  if(dramfs_check_fd(fd) == 0) {
+    uint8_t *data = (uint8_t *)ptr;
+
+    // Return early on len == 0
+    if (len == 0) return (ssize_t) 0;
+
+    // We buffer until len or newline, which is standard behavior for a console
+    int ch;
+    // Read just 1 character to start
+    while ((ch = dramfs_getchar()) == -1);
+    int i = 0;
+    do {
+      data[i++] = ch;
+      if (i == len) break;
+    } while ((ch = dramfs_getchar()) != -1);
+
+    return (ssize_t) i;
+  }
+
   lfs_file_t *fptr = dramfs_get_file(fd);
   return (ssize_t) lfs_file_read(&dramfs_fs, fptr, ptr, (lfs_size_t) len);
 }
