e30d86e47d25cf8976c3cd321a05aabdaa268047
Farzam Gilani <gilanifarzam@gmail.com>
2024-03-04 12:32:32 -0800

    Call dramfs_getchar if stdin not initialized

--- libgloss/dramfs/sys_read.c
+++ libgloss/dramfs/sys_read.c
@@ -7,34 +7,42 @@
 ssize_t _read(int fd, void *ptr, size_t len)
 {
   if(dramfs_check_fd(fd) < 0) {
-    return -1;
+    // Read from host if stdin not initialized
+    if(fd == 0)
+      goto host_getchar;
+    else
+      return -1;
   }
 
-  if(fd == 0) {
-    uint8_t *data = (uint8_t *)ptr;
-
-    // Return early on len == 0
-    if (len == 0) return (ssize_t) 0;
-
-    int ch;
-    if (dramfs_nonblock_fd(fd) == 0) {
-      // Block to read just 1 character to start
-      while ((ch = dramfs_getchar()) == -1);
-    } else {
-      // Read the first character, and return immediately if it's EOF
-      if ((ch = dramfs_getchar()) == -1) return (ssize_t) 0;
-    }
-
-    // Keep reading until new
-    int i = 0;
-    do {
-      data[i++] = ch;
-      if (i == len) break;
-    } while ((ch = dramfs_getchar()) != -1);
-
-    return (ssize_t) i;
-  }
-
+  // Read from LFS file
   lfs_file_t *fptr = dramfs_get_file(fd);
-  return (ssize_t) lfs_file_read(&dramfs_fs, fptr, ptr, (lfs_size_t) len);
+  ssize_t ret = lfs_file_read(&dramfs_fs, fptr, ptr, (lfs_size_t) len);
+
+  // Fall through if stdin is empty otherwise return
+  if(!((fd == 0) && (ret == 0)))
+    return ret;
+
+host_getchar:;
+  uint8_t *data = (uint8_t *)ptr;
+
+  // Return early on len == 0
+  if (len == 0) return (ssize_t) 0;
+
+  int ch;
+  if (dramfs_nonblock_fd(fd) == 0) {
+    // Block to read just 1 character to start
+    while ((ch = dramfs_getchar()) == -1);
+  } else {
+    // Read the first character, and return immediately if it's EOF
+    if ((ch = dramfs_getchar()) == -1) return (ssize_t) 0;
+  }
+
+  // Keep reading until new
+  int i = 0;
+  do {
+    data[i++] = ch;
+    if (i == len) break;
+  } while ((ch = dramfs_getchar()) != -1);
+
+  return (ssize_t) i;
 }
